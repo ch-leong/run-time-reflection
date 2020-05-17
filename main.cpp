4659fca1ec73c52b0eab81b99da98a1551edd04a
Chee Hwa, Leong <cheehwa.leong@knorex.com>
2020-05-17 11:00:44 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "component.h"
+
+std::vector<std::string> TokenizeString(std::string const& string)
+{
+    std::istringstream iss(string);
+    std::vector<std::string> results(std::istream_iterator<std::string>{iss},
+        std::istream_iterator<std::string>());
+    return std::move(results);
+}
+
+int main()
+{
+    Transform test("test_name");
+    std::cout << "Transform member variables are: \n";
+    auto& variables = test.GetVariables();
+    for (auto const& variable : variables)
+    {
+        std::cout << variable->GetName() << "\n";
+    }
+    system("pause");
+}
